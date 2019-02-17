2a2f3505a803e363226df576ee3c37e55cbc52c4
Andrew Edson <docedson@gmail.com>
2019-02-17 10:59:35 -0600

    2-17-19Update1

--- classwork/Quiz/Lengths.cs
+++ classwork/Quiz/Lengths.cs
@@ -12,27 +12,27 @@ namespace Quiz
             switch(feetChange)
             {
                 case Measurements.Millimeters:
-                double total = Math.Floor(feet * 304.8) / 100;
+                double total = Math.Round((feet * 304.8), 2);
                 Console.WriteLine(" " +total + " " + Measurements.Millimeters);
                 break;                    
 
                 case Measurements.Centimeters:
-                double total1 = Math.Floor(feet * 30.48) / 100;
+                double total1 = Math.Round((feet * 30.48), 2);
                 Console.WriteLine(" " +total1 + " " + Measurements.Centimeters);
                 break;
 
                 case Measurements.Inches:
-                double total2 = Math.Floor(feet * 12) / 100;
+                double total2 = Math.Round((feet * 12), 2);
                 Console.WriteLine(" " +total2 + " " + Measurements.Inches);
                 break;
 
                 case Measurements.Meters:
-                double total3 = Math.Floor(feet * 0.3048) / 100;
+                double total3 = Math.Round((feet * 0.3048), 2);
                 Console.WriteLine(" " +total3 + " " + Measurements.Meters);
                 break;
 
                 case Measurements.Yards:
-                double total4 = Math.Floor(feet * 0.33333333333) / 100;
+                double total4 = Math.Round((feet * 0.33333333333), 2);
                 Console.WriteLine(" and " + total4 + " " + Measurements.Yards);
                 break;
 
